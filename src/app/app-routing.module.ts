959f7daaf8dc7e49b2005a46874ec18172d99138
Ahoud-AlSabbari <ahoud.alsabbari@gmail.com>
2023-09-11 15:56:02 +0400

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './home/login/login.component';
+
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
